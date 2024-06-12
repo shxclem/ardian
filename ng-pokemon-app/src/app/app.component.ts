3fd9b31c05f7be293bf890f48a93af742a2bd459
shxclem <durecuclem@cy-tech.fr>
2024-06-12 17:38:38 +0200

    Création du tableau de Pokemons avec images et css

---
+++ ng-pokemon-app/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent implements OnInit{
+  pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon|undefined;
+
+  ngOnInit(): void {
+    console.table(this.pokemonList);
+  }
+
+  selectPokemon(pokemonId: string) {
+    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)!;
+    if(pokemon) {
+      console.log("Vous avez demandé le pokemon " + pokemon.name);
+      this.pokemonSelected = pokemon;
+    }
+    else {
+      console.log("Vous avez demandé un pokemon qui n'existe pas");
+      this.pokemonSelected = pokemon;
+    }
+  } 
+}
---
+++ ng-pokemon-app/src/app/mock-pokemon-list.ts
@@ -0,0 +1,112 @@
+import { Pokemon } from './pokemon';
+  
+export const POKEMONS: Pokemon[] = [
+    {
+        id: 1,
+        hp: 25,
+        cp: 5,
+        name: "Bulbizarre",
+        picture: "https://www.pokepedia.fr/images/e/ef/Bulbizarre-RFVF.png",
+        types: ["Plante", "Poison"],
+        created: new Date() 
+    },
+    {
+        id: 2,
+        hp: 28,
+        cp: 6,
+        name: "Salamèche",
+        picture: "https://www.pokepedia.fr/images/8/89/Salam%C3%A8che-RFVF.png",
+        types: ["Feu"],
+        created: new Date()
+    },
+    {
+        id: 3,
+        hp: 21,
+        cp: 4,
+        name: "Carapuce",
+        picture: "https://www.pokepedia.fr/images/c/cc/Carapuce-RFVF.png",
+        types: ["Eau"],
+        created: new Date()
+    },
+    {
+        id: 4,
+        hp: 30,
+        cp: 7,
+        name: "Aspicot",
+        picture: "https://www.pokepedia.fr/images/9/9b/Aspicot-RFVF.png",
+        types: ["Insecte", "Poison"],
+        created: new Date()
+    },
+    {
+        id: 5,
+        hp: 16,
+        cp: 2,
+        name: "Roucool",
+        picture: "https://www.pokepedia.fr/images/9/94/Roucool-RFVF.png",
+        types: ["Normal", "Vol"],
+        created: new Date()
+    },
+    {
+        id: 6,
+        hp: 18,
+        cp: 3,
+        name: "Rattata",
+        picture: "https://www.pokepedia.fr/images/9/9e/Rattata-RFVF.png",
+        types: ["Normal"],
+        created: new Date()
+    },
+    {
+        id: 7,
+        hp: 14,
+        cp: 2,
+        name: "Piafabec",
+        picture: "https://www.pokepedia.fr/images/7/7d/Piafabec-RFVF.png",
+        types: ["Normal", "Vol"],
+        created: new Date()
+    },
+    {
+        id: 8,
+        hp: 16,
+        cp: 2,
+        name: "Abo",
+        picture: "https://www.pokepedia.fr/images/5/56/Abo-RFVF.png",
+        types: ["Poison"],
+        created: new Date()
+    },
+    {
+        id: 9,
+        hp: 13,
+        cp: 2,
+        name: "Pikachu",
+        picture: "https://www.pokepedia.fr/images/7/76/Pikachu-DEPS.png",
+        types: ["Electrik"],
+        created: new Date()
+    },
+    {
+        id: 10,
+        hp: 30,
+        cp: 10,
+        name: "Sabelette",
+        picture: "https://www.pokepedia.fr/images/6/66/Sabelette-RFVF.png",
+        types: ["Normal"],
+        created: new Date()
+    },
+    {
+        id: 11,
+        hp: 21,
+        cp: 6,
+        name: "Mélofée",
+        picture: "https://www.pokepedia.fr/images/4/4d/M%C3%A9lof%C3%A9e-RFVF.png",
+        types: ["Fée"],
+        created: new Date()
+    },
+    {
+        id: 12,
+        hp: 17,
+        cp: 8,
+        name: "Goupix",
+        picture: "https://www.pokepedia.fr/images/f/f6/Goupix-RFVF.png",
+        types: ["Feu"], 
+        created: new Date()
+    }
+]
